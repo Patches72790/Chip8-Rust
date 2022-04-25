00e38b589b1512572a94f67277f604ef4c307073
Patrick <patches72790@gmail.com>
2022-04-25 08:38:10 -0500

    linted

--- www/index.js
+++ www/index.js
@@ -4,7 +4,7 @@ const container = document.getElementById('container');
 const cpu = chip8.Cpu.new();
 
 const renderLoop = () => {
-    container.textContent = cpu.render();
+  container.textContent = cpu.render();
   cpu.tick();
 
   requestAnimationFrame(renderLoop);
