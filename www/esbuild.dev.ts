8491f609e2a7f5063ee22ee239a3698515f50d9c
Patrick <patches72790@gmail.com>
2023-01-29 23:50:22 -0600

    fixed ts-loader issue

--- www/esbuild.dev.ts
+++
@@ -1,9 +0,0 @@
-import esbuild from "esbuild";
-import wasmLoader from "esbuild-plugin-wasm";
-
-await esbuild.build({
-  entryPoints: ["./bootstrap.js"],
-  bundle: true,
-  outfile: "./dist/bootstrap.js",
-  plugins: [wasmLoader()],
-});
--- www/webpack.common.js
+++ www/webpack.common.js
@@ -2,16 +2,25 @@ const CopyWebpackPlugin = require("copy-webpack-plugin");
 const path = require("path");
 
 module.exports = {
-  entry: "./public/bootstrap.js",
+  entry: "./src/index.ts",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bootstrap.js",
   },
   plugins: [
-    new CopyWebpackPlugin(["./public/index.html", "./public/index.css"]),
+    new CopyWebpackPlugin([
+      "./public/index.html",
+      "./public/index.css",
+      "./public/favicon.ico",
+    ]),
   ],
-  module: { rules: [{ test: "/.ts$/", loader: "ts-loader" }] },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  module: {
+    rules: [{ test: /\.ts$/, loader: "ts-loader", exclude: /node_modules/ }],
+  },
   experiments: {
-    syncWebAssembly: true,
+    asyncWebAssembly: true,
   },
 };
